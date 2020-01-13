bbfd052194099ce7131bb407024669ca9a5d7b5f
fennieer <erpeifen09@gmail.com>
2020-01-13 23:01:36 +0800

    add my-angular-site tutorial

---
+++ my-angular-site/src/app/users/user-single/user-single.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from 'src/app/core/services/user.service';
+
+@Component({
+  selector: 'app-user-single',
+  templateUrl: './user-single.component.html',
+  styleUrls: ['./user-single.component.scss']
+})
+export class UserSingleComponent implements OnInit {
+  user;
+
+  constructor(
+    private userService: UserService,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit() {
+    //grab the username out of the url
+    this.route.params.subscribe(params => {
+      const username = params['username'];
+
+    //use the userservice to get data from github api
+    this.userService
+      .getUser(username)
+      .subscribe(user => this.user = user);
+
+    })
+  //bind that to a user variable
+  }
+
+
+}
